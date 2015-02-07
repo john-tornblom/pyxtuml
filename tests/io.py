094481c0180e060ef3ec3a41727ca8122005f6da
John Törnblom <john.tornblom@gmail.com>
2015-02-07 14:08:08 +0100

    io: added test cases for loading tables with keywords as table names

--- tests/io.py
+++ tests/io.py
@@ -8,6 +8,16 @@ from xtuml import model
 from xtuml import io
 
 
+def load(fn):
+    def load_wrapper(self, *args, **kwargs):
+        self.loader.input(fn.__doc__)
+        id_generator = model.IdGenerator()
+        metamodel = self.loader.build_metamodel(id_generator)
+        fn(self, metamodel)
+    
+    return load_wrapper
+
+
 class TestLoader(unittest.TestCase):
 
     def setUp(self):
@@ -17,7 +27,7 @@ class TestLoader(unittest.TestCase):
     def tearDown(self):
         del self.loader
 
-    def testLoadGlobals(self):
+    def testFileInput(self):
         base_dir = '%s/../resources' % os.path.dirname(__file__)
         self.loader.filename_input('%s/ooaofooa_schema.sql' % base_dir)
         self.loader.filename_input('%s/Globals.xtuml' % base_dir)
@@ -26,6 +36,87 @@ class TestLoader(unittest.TestCase):
         metamodel = self.loader.build_metamodel(id_generator)
         self.assertTrue(metamodel.select_any('S_DT', Name='integer') is not None)
         
+    @load
+    def testTableNamedCREATE(self, m):
+        '''
+        CREATE TABLE CREATE (Id UNIQUE_ID);
+        INSERT INTO CREATE VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('CREATE') is not None)
+        
+    @load
+    def testTableNamedTABLE(self, m):
+        '''
+        CREATE TABLE TABLE (Id UNIQUE_ID);
+        INSERT INTO TABLE VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('TABLE') is not None)
+        
+        
+    @load
+    def testTableNamedINSERT(self, m):
+        '''
+        CREATE TABLE INSERT (Id UNIQUE_ID);
+        INSERT INTO INSERT VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('INSERT') is not None)
+        
+    @load
+    def testTableNamedINTO(self, m):
+        '''
+        CREATE TABLE INTO (Id UNIQUE_ID);
+        INSERT INTO INTO VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('INTO') is not None)
+        
+    @load
+    def testTableNamedVALUES(self, m):
+        '''
+        CREATE TABLE VALUES (Id UNIQUE_ID);
+        INSERT INTO VALUES VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('VALUES') is not None)
+        
+    @load
+    def testTableNamedROP(self, m):
+        '''
+        CREATE TABLE ROP (Id UNIQUE_ID);
+        INSERT INTO ROP VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('ROP') is not None)
+        
+    @load
+    def testTableNamedREF_ID(self, m):
+        '''
+        CREATE TABLE REF_ID (Id UNIQUE_ID);
+        INSERT INTO REF_ID VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('REF_ID') is not None)
+        
+    @load
+    def testTableNamedFROM(self, m):
+        '''
+        CREATE TABLE FROM (Id UNIQUE_ID);
+        INSERT INTO FROM VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('FROM') is not None)
+        
+    @load
+    def testTableNamedTO(self, m):
+        '''
+        CREATE TABLE TO (Id UNIQUE_ID);
+        INSERT INTO TO VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('TO') is not None)
+        
+    @load
+    def testTableNamedPHRASE(self, m):
+        '''
+        CREATE TABLE PHRASE (Id UNIQUE_ID);
+        INSERT INTO PHRASE VALUES ("00000000-0000-0000-0000-000000000000");
+        '''
+        self.assertTrue(m.select_any('PHRASE') is not None)
+
 def suite():
     loader = unittest.TestLoader()
     s = loader.loadTestsFromTestCase(TestLoader)
