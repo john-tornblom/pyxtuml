548a127ee26e80f6840e16d7a2c0daa64786c1b5
John Törnblom <john.tornblom@gmail.com>
2015-04-14 22:53:16 +0200

    examples: added initial support for data types to the xsd schema generator

--- examples/export_model_as_xsd.py
+++ examples/export_model_as_xsd.py
@@ -14,50 +14,71 @@ sys.path.append(base_dir)
 from xtuml import io
 
 
-def get_type_name(m, o_attr):
-    s_dt = m.navigate_one(o_attr).S_DT[114]()
-    while m.navigate_one(s_dt).S_UDT[17]():
-        s_dt = m.navigate_one(s_dt).S_UDT[17].S_DT[18]()
-        
-    if   s_dt.name == 'string': return 'string'
-    elif s_dt.name == 'integer': return 'integer'
-    elif s_dt.name == 'real': return 'decimal'
-    elif s_dt.name == 'boolean': return 'boolean'
-    elif s_dt.name == 'unique_id': return 'integer'
+def get_type_name(m, s_dt):
+    '''
+    get the xsd name of a S_DT
+    '''
+    s_cdt = m.navigate_one(s_dt).S_CDT[17]()
+    if s_cdt and s_cdt.core_typ in range(1, 6):
+        return s_dt.name
     
     s_edt = m.navigate_one(s_dt).S_EDT[17]()
     if s_edt:
-        return s_edt.name
-
-    o_attr = m.navigate_one(o_attr).O_RATTR[106].O_BATTR[113].O_ATTR[106]()
-    if o_attr:
-        return get_type_name(m, o_attr)
+        return s_dt.name
     
+    s_udt = m.navigate_one(s_dt).S_UDT[17]()
+    if s_udt:
+        return s_dt.name
 
+#    s_sdt = m.navigate_one(s_dt).S_SDT[17]()
+#    if s_sdt:
+#        return s_dt.name
+    
+    
+def get_refered_attribute(m, o_attr):
+    '''
+    Get the the referred attribute.
+    '''
+    o_attr_ref = m.navigate_one(o_attr).O_RATTR[106].O_BATTR[113].O_ATTR[106]()
+    if o_attr_ref:
+        return get_refered_attribute(m, o_attr_ref)
+    else:
+        return o_attr
+    
+    
 def is_contained_in(m, pe_pe, root):
+    '''
+    Determine if a PE_PE is contained within a EP_PKG or a C_C.
+    '''
     if not pe_pe:
         return False
     
     if pe_pe.__class__.__name__ != 'PE_PE':
         pe_pe = m.navigate_one(pe_pe).PE_PE[8001]()
-
+    
     ep_pkg = m.navigate_one(pe_pe).EP_PKG[8000]()
     c_c = m.navigate_one(pe_pe).C_C[8003]()
     
     if root in [ep_pkg, c_c]:
         return True
+    
     elif is_contained_in(m, ep_pkg, root):
         return True
+    
     elif is_contained_in(m, c_c, root):
         return True
+    
     else:
         return False
 
 
 def is_global(m, pe_pe):
+    '''
+    Check if a PE_PE is globally defined, i.e. not inside a C_C
+    '''
     if pe_pe.__class__.__name__ != 'PE_PE':
         pe_pe = m.navigate_one(pe_pe).PE_PE[8001]()
-
+    
     if m.navigate_one(pe_pe).C_C[8003]():
         return False
     
@@ -67,29 +88,148 @@ def is_global(m, pe_pe):
     
     return is_global(m, pe_pe)
 
-    
-def build_class(m, o_obj):
-    cls = ET.Element('element', name=o_obj.key_lett, maxOccurs='unbounded')
-    attributes = ET.SubElement(cls, 'complexType')
-    for o_attr in m.navigate_many(o_obj).O_ATTR[102]():
-        type_name = get_type_name(m, o_attr)
-        if type_name:
-            ET.SubElement(attributes, 'attribute', name=o_attr.name, type=type_name)
 
+def build_core_type(m, s_cdt):
+    '''
+    Build an xsd simpleType out of a S_CDT.
+    '''
+    s_dt = m.navigate_one(s_cdt).S_DT[17]()
+    
+    if s_dt.name == 'void':
+        type_name = None
+    
+    elif s_dt.name == 'boolean':
+        type_name = 'xs:boolean'
+    
+    elif s_dt.name == 'integer':
+        type_name = 'xs:integer'
+    
+    elif s_dt.name == 'real':
+        type_name = 'xs:decimal'
+    
+    elif s_dt.name == 'string':
+        type_name = 'xs:string'
+    
+    elif s_dt.name == 'unique_id':
+        type_name = 'xs:integer'
+    
+    else:
+        type_name = None
+    
+    if type_name:
+        mapped_type = ET.Element('xs:simpleType', name=s_dt.name)
+        ET.SubElement(mapped_type, 'xs:restriction', base=type_name)
+        return mapped_type
+
+
+def build_enum_type(m, s_edt):
+    '''
+    Build an xsd simpleType out of a S_EDT.
+    '''
+    s_dt = m.navigate_one(s_edt).S_DT[17]()
+    enum = ET.Element('xs:simpleType', name=s_dt.name)
+    enum_list = ET.SubElement(enum, 'xs:restriction', base='xs:string')
+    
+    first_filter = lambda selected: not m.navigate_one(selected).S_ENUM[56, 'precedes']()
+    
+    s_enum = m.navigate_any(s_edt).S_ENUM[27](first_filter)
+    while s_enum:
+        ET.SubElement(enum_list, 'xs:enumeration', value=s_enum.name)
+        s_enum = m.navigate_one(s_enum).S_ENUM[56, 'succeeds']()
+    
+    return enum
+
+
+def build_struct_type(m, s_sdt):
+    '''
+    Build an xsd complexType out of a S_SDT.
+    '''
+    s_dt = m.navigate_one(s_sdt).S_DT[17]()
+    struct = ET.Element('xs:complexType', name=s_dt.name)
+    
+    first_filter = lambda selected: not m.navigate_one(selected).S_MBR[46, 'precedes']()
+    
+    s_mbr = m.navigate_any(s_sdt).S_MBR[44](first_filter)
+    while s_mbr:
+        s_dt = m.navigate_one(s_mbr).S_DT[45]()
+        type_name = get_type_name(m, s_dt)
+        ET.SubElement(struct, 'xs:attribute', name=s_mbr.name, type=type_name)
+        s_mbr = m.navigate_one(s_mbr).S_MBR[46, 'succeeds']()
+    
+    return struct
+
+
+def build_user_type(m, s_udt):
+    '''
+    Build an xsd simpleType out of a S_UDT.
+    '''
+    s_dt_user = m.navigate_one(s_udt).S_DT[17]()
+    s_dt_base = m.navigate_one(s_udt).S_DT[18]()
+    
+    base_name = get_type_name(m, s_dt_base)
+    if base_name:
+        user = ET.Element('xs:simpleType', name=s_dt_user.name)
+        ET.SubElement(user, 'xs:extension', base=base_name)
+        
+        return user
+
+
+def build_type(m, s_dt):
+    '''
+    Build a partial xsd tree out of a S_DT and its sub types S_CDT, S_EDT, S_SDT and S_UDT.
+    '''
+    s_cdt = m.navigate_one(s_dt).S_CDT[17]()
+    if s_cdt:
+        return build_core_type(m, s_cdt)
+    
+    s_edt = m.navigate_one(s_dt).S_EDT[17]()
+    if s_edt:
+        return build_enum_type(m, s_edt)
+    
+    s_udt = m.navigate_one(s_dt).S_UDT[17]()
+    if s_udt:
+        return build_user_type(m, s_udt)
+    
+#    s_sdt = m.navigate_one(s_dt).S_SDT[17]()
+#    if s_sdt:
+#        return build_struct_type(m, s_sdt)
+
+
+def build_class(m, o_obj):
+    '''
+    Build an xsd complex element out of a O_OBJ, including its O_ATTR.
+    '''
+    cls = ET.Element('xs:element', name=o_obj.key_lett, maxOccurs='unbounded')
+    attributes = ET.SubElement(cls, 'xs:complexType')
+    for o_attr in m.navigate_many(o_obj).O_ATTR[102]():
+        o_attr_ref = get_refered_attribute(m, o_attr)
+        s_dt = m.navigate_one(o_attr_ref).S_DT[114]()
+        while m.navigate_one(s_dt).S_UDT[17]():
+            s_dt = m.navigate_one(s_dt).S_UDT[17].S_DT[18]()
+        
+        type_name = get_type_name(m, s_dt)
+        if type_name:
+            ET.SubElement(attributes, 'xs:attribute', name=o_attr.name, type=type_name)
+    
     return cls
 
 
 def build_component(m, c_c):
-    component = ET.Element('element', name=c_c.name)
+    '''
+    Build an xsd complex element out of a C_C, including its packaged S_DT and O_OBJ.
+    '''
+    component = ET.Element('xs:element', name=c_c.name)
     
-    classes = ET.SubElement(component, 'complexType')
-    classes = ET.SubElement(classes, 'sequence')
-        
+    classes = ET.SubElement(component, 'xs:complexType')
+    classes = ET.SubElement(classes, 'xs:sequence')
+    
     scope_filter = lambda selected: is_contained_in(m, selected, c_c)
+    
     for s_dt in m.select_many('S_DT', scope_filter):
-        # TODO: data types
-        pass
-            
+        datatype = build_type(m, s_dt)
+        if datatype is not None:
+            schema.append(datatype)
+    
     for o_obj in m.select_many('O_OBJ', scope_filter):
         cls = build_class(m, o_obj)
         classes.append(cls)
@@ -98,22 +238,29 @@ def build_component(m, c_c):
 
 
 def build_schema(m):
-    schema = ET.Element('schema')
-    schema.set('xmlns', 'http://www.w3.org/2001/XMLSchema')
-    
+    '''
+    Build an xsd schema from a bridgepoint model.
+    '''
+    schema = ET.Element('xs:schema')
+    schema.set('xmlns:xs', 'http://www.w3.org/2001/XMLSchema')
+
     global_filter = lambda selected: is_global(m, selected)
     for s_dt in m.select_many('S_DT', global_filter):
-        # TODO: data types
-        pass
+        datatype = build_type(m, s_dt)
+        if datatype is not None:
+            schema.append(datatype)
     
     for c_c in m.select_many('C_C'):
         component = build_component(m, c_c)
         schema.append(component)
-
+    
     return schema
 
 
 def prettify(xml_string):
+    '''
+    Indent an xml string with four spaces, and add an additional line break after each node.
+    '''
     reparsed = xml.dom.minidom.parseString(xml_string)
     return reparsed.toprettyxml(indent="    ")
 
@@ -122,7 +269,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('usage: %s <path to prebuild model.sql>' % sys.argv[0])
         sys.exit()
-        
+    
     loader = io.load.ModelLoader()
     loader.build_parser()
     loader.filename_input('%s/resources/ooaofooa_schema.sql' % base_dir)
