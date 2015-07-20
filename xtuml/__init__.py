f9de8933628c74f05d9530b7e99bec18cc997af3
John Törnblom <john.tornblom@gmail.com>
2015-07-20 11:13:34 +0200

    xtuml: expose useful classes defined in the model module to the xtuml package

--- xtuml/__init__.py
+++ xtuml/__init__.py
@@ -13,9 +13,17 @@ from .persist import serialize_metamodel
 from .persist import serialize_instance
 from .persist import serialize_value
 
-from .model import MetaModel
+from .model import AssociationLink
+from .model import SingleAssociationLink
+from .model import ManyAssociationLink
+
+from .model import QuerySet
 from .model import IdGenerator
+from .model import UUIDGenerator
+from .model import IntegerGenerator
 from .model import ModelException
+from .model import MetaModel
+
 from .model import navigate_any
 from .model import navigate_one
 from .model import navigate_many
