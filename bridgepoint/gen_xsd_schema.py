00e76e9e31251bdc7a0f5b6756e8e5baf8544250
John Törnblom <john.tornblom@gmail.com>
2015-08-11 22:32:09 +0200

    bridgepoint xsd schema: use ooaofooa loader

--- bridgepoint/gen_xsd_schema.py
+++ bridgepoint/gen_xsd_schema.py
@@ -19,75 +19,10 @@ import xtuml
 from xtuml import navigate_any as nav_any
 from xtuml import navigate_any as nav_one
 from xtuml import navigate_many as nav_many
+from bridgepoint import ooaofooa
 
 
 logger = logging.getLogger('gen_xsd_schema')
-
-
-# parts of the meta model for BridgePoint (ooaofooa) which are used by this script
-bp_schema = '''
-CREATE TABLE PE_PE (Element_ID UNIQUE_ID,Visibility INTEGER,Package_ID UNIQUE_ID,Component_ID UNIQUE_ID,type INTEGER);
-CREATE TABLE C_C (Id UNIQUE_ID,Package_ID UNIQUE_ID,NestedComponent_Id UNIQUE_ID,Name STRING,Descrip STRING,Mult INTEGER,Root_Package_ID UNIQUE_ID,isRealized BOOLEAN,Realized_Class_Path STRING);
-CREATE TABLE EP_PKG (Package_ID UNIQUE_ID,Sys_ID UNIQUE_ID,Direct_Sys_ID UNIQUE_ID,Name STRING,Descrip STRING,Num_Rng INTEGER);
-CREATE TABLE S_DT (DT_ID UNIQUE_ID,Dom_ID UNIQUE_ID,Name STRING,Descrip STRING,DefaultValue STRING);
-CREATE TABLE S_CDT (DT_ID UNIQUE_ID,Core_Typ INTEGER);
-CREATE TABLE S_EDT (DT_ID UNIQUE_ID);
-CREATE TABLE S_ENUM (Enum_ID UNIQUE_ID,Name STRING,Descrip STRING,EDT_DT_ID UNIQUE_ID,Previous_Enum_ID UNIQUE_ID);
-CREATE TABLE S_UDT (DT_ID UNIQUE_ID,CDT_DT_ID UNIQUE_ID,Gen_Type INTEGER);
-CREATE TABLE S_SDT (DT_ID UNIQUE_ID);
-CREATE TABLE S_MBR (Member_ID UNIQUE_ID,Name STRING,Descrip STRING,Parent_DT_DT_ID UNIQUE_ID,DT_ID UNIQUE_ID,Previous_Member_ID UNIQUE_ID,Dimensions STRING);
-CREATE TABLE O_OBJ (Obj_ID UNIQUE_ID,Name STRING,Numb INTEGER,Key_Lett STRING,Descrip STRING,SS_ID UNIQUE_ID);
-CREATE TABLE O_ATTR (Attr_ID UNIQUE_ID,Obj_ID UNIQUE_ID,PAttr_ID UNIQUE_ID,Name STRING,Descrip STRING,Prefix STRING,Root_Nam STRING,Pfx_Mode INTEGER,DT_ID UNIQUE_ID,Dimensions STRING,DefaultValue STRING);
-CREATE TABLE O_RATTR (Attr_ID UNIQUE_ID,Obj_ID UNIQUE_ID,BAttr_ID UNIQUE_ID,BObj_ID UNIQUE_ID,Ref_Mode INTEGER,BaseAttrName STRING);
-CREATE TABLE O_BATTR (Attr_ID UNIQUE_ID,Obj_ID UNIQUE_ID);
-CREATE TABLE O_NBATTR (Attr_ID UNIQUE_ID,Obj_ID UNIQUE_ID);
-
-CREATE ROP REF_ID R102 FROM MC O_ATTR (Obj_ID) TO 1 O_OBJ (Obj_ID);
-CREATE ROP REF_ID R103 FROM 1C O_ATTR (PAttr_ID,Obj_ID) PHRASE 'succeeds' TO 1C O_ATTR (Attr_ID,Obj_ID) PHRASE 'precedes';
-CREATE ROP REF_ID R113 FROM MC O_RATTR (BAttr_ID,BObj_ID) TO 1 O_BATTR (Attr_ID,Obj_ID);
-CREATE ROP REF_ID R114 FROM MC O_ATTR (DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R106 FROM 1C O_BATTR (Attr_ID,Obj_ID) TO 1 O_ATTR (Attr_ID,Obj_ID);
-CREATE ROP REF_ID R106 FROM 1C O_RATTR (Attr_ID,Obj_ID) TO 1 O_ATTR (Attr_ID,Obj_ID);
-CREATE ROP REF_ID R107 FROM 1C O_NBATTR (Attr_ID,Obj_ID) TO 1 O_BATTR (Attr_ID,Obj_ID);
-CREATE ROP REF_ID R17  FROM 1C S_CDT (DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R17  FROM 1C S_UDT (DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R17  FROM 1C S_EDT (DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R18  FROM MC S_UDT (CDT_DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R27  FROM MC S_ENUM (EDT_DT_ID) TO 1 S_EDT (DT_ID);
-CREATE ROP REF_ID R56  FROM 1C S_ENUM (Previous_Enum_ID) PHRASE 'succeeds' TO 1C S_ENUM (Enum_ID) PHRASE 'precedes';
-CREATE ROP REF_ID R44  FROM MC S_MBR (Parent_DT_DT_ID) TO 1 S_SDT (DT_ID);
-CREATE ROP REF_ID R45  FROM MC S_MBR (DT_ID) TO 1 S_DT (DT_ID);
-CREATE ROP REF_ID R46  FROM 1C S_MBR (Previous_Member_ID,Parent_DT_DT_ID) PHRASE 'succeeds' TO 1C S_MBR (Member_ID,Parent_DT_DT_ID) PHRASE 'precedes';
-CREATE ROP REF_ID R8000 FROM MC PE_PE (Package_ID) TO 1C EP_PKG (Package_ID);
-CREATE ROP REF_ID R8001 FROM 1C S_DT ( DT_ID ) TO 1 PE_PE (Element_ID);
-CREATE ROP REF_ID R8001 FROM 1C EP_PKG (Package_ID) TO 1 PE_PE (Element_ID);
-CREATE ROP REF_ID R8001 FROM 1C C_C (Id) TO 1 PE_PE (Element_ID);
-CREATE ROP REF_ID R8001 FROM 1C O_OBJ (Obj_ID) TO 1 PE_PE (Element_ID);
-CREATE ROP REF_ID R8003 FROM MC PE_PE (Component_ID) TO 1C C_C (Id);
-'''
-
-# global data types used by BridgePoint.
-bp_globals = '''
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000000", 0);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000001", 1, "00000000-0000-0000-0000-000000000000", "00000000-0000-0000-0000-000000000000", 3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000001", "00000000-0000-0000-0000-000000000000", 'boolean', '', '');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000001",1);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000002",1,"00000000-0000-0000-0000-000000000000","00000000-0000-0000-0000-000000000000",3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000002","00000000-0000-0000-0000-000000000000",'integer','','');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000002",2);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000003",1,"00000000-0000-0000-0000-000000000000","00000000-0000-0000-0000-000000000000",3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000003","00000000-0000-0000-0000-000000000000",'real','','');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000003",3);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000004",1,"00000000-0000-0000-0000-000000000000","00000000-0000-0000-0000-000000000000",3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000004","00000000-0000-0000-0000-000000000000",'string','','');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000004",4);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000005",1,"00000000-0000-0000-0000-000000000000","00000000-0000-0000-0000-000000000000",3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000005","00000000-0000-0000-0000-000000000000",'unique_id','','');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000005",5);
-INSERT INTO PE_PE VALUES ("ba5eda7a-def5-0000-0000-000000000007",1,"00000000-0000-0000-0000-000000000000","00000000-0000-0000-0000-000000000000",3);
-INSERT INTO S_DT  VALUES ("ba5eda7a-def5-0000-0000-000000000007","00000000-0000-0000-0000-000000000000",'same_as<Base_Attribute>','','');
-INSERT INTO S_CDT VALUES ("ba5eda7a-def5-0000-0000-000000000007",7);
-'''
 
 
 def get_type_name(s_dt):
@@ -313,19 +248,10 @@ def gen_schema():
         
     logging.basicConfig(level=logging.INFO)
     
-    loader = xtuml.ModelLoader()
-    loader.build_parser()
-    loader.input(bp_schema)
-    loader.input(bp_globals)
+    loader = ooaofooa.Loader()
     
-    for arg in args:
-        if os.path.isdir(arg):
-            for path, _, files in os.walk(arg):
-                for name in files:
-                    if name.endswith('.xtuml'):
-                        loader.filename_input(os.path.join(path, name))
-        else:
-            loader.filename_input(arg)
+    for filename in args:
+        loader.filename_input(filename)
     
     m = loader.build_metamodel(ignore_undefined_classes=True)
     c_c = m.select_any('C_C', lambda inst: inst.Name == opts.component)
