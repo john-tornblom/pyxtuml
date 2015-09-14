80919a72d6174358b069796481edf04bccece887
John Törnblom <john.tornblom@gmail.com>
2015-09-14 18:59:42 +0200

    examples: show how to list enums in the proper order

---
+++ examples/list_bp_enums.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+
+import sys
+
+from xtuml import navigate_one as one
+from xtuml import navigate_many as many
+from bridgepoint import ooaofooa
+
+
+if len(sys.argv) < 2:
+    print('')
+    print('  usage: %s <path to bridgepoint model folder>' % sys.argv[0])
+    print('')
+    sys.exit(1)
+
+    
+loader = ooaofooa.Loader()
+loader.filename_input(sys.argv[1])
+m = loader.build_metamodel(ignore_undefined_classes=True)
+
+
+get_name = lambda inst: one(inst).S_DT[17]().Name
+for s_edt in sorted(m.select_many('S_EDT'), key=get_name):
+    print(get_name(s_edt))
+
+    is_first = lambda inst: not one(inst).S_ENUM[56, 'precedes']()
+    s_enum = one(s_edt).S_ENUM[27](is_first)
+    while s_enum:
+        print('    %s' % s_enum.Name)
+        s_enum = one(s_enum).S_ENUM[56, 'succeeds']()
+
