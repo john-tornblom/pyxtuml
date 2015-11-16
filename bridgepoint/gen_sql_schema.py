01b93c7bd750bd4249a2a5e6fdf5a890d3f81799
John Törnblom <john.tornblom@gmail.com>
2015-11-16 21:54:16 +0100

    gen_sql_schema: expose logging verbosity to cli

--- bridgepoint/gen_sql_schema.py
+++ bridgepoint/gen_sql_schema.py
@@ -265,12 +265,22 @@ def main():
                       help="save sql schema to PATH (required)",
                       action="store", default=None)
     
+    parser.add_option("-v", "--verbosity", dest='verbosity', action="count", 
+                      help="increase debug logging level", default=2)
+
+    
     (opts, args) = parser.parse_args()
     if len(args) == 0 or opts.output is None:
         parser.print_help()
         sys.exit(1)
-        
-    logging.basicConfig(level=logging.INFO)
+
+    levels = {
+              0: logging.ERROR,
+              1: logging.WARNING,
+              2: logging.INFO,
+              3: logging.DEBUG,
+    }
+    logging.basicConfig(level=levels.get(opts.verbosity, logging.DEBUG))
 
     loader = ooaofooa.Loader()
     for filename in args:
