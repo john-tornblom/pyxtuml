91b825f30a86373df8440b7d1fc49a6ac1340f9e
John Törnblom <john.tornblom@gmail.com>
2015-12-27 13:27:11 +0100

    test: added test cases for xtuml.tools

---
+++ tests/test_tools.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+import sys
+import unittest
+
+import xtuml.tools
+
+
+class Node(object):
+    '''
+    Test Node used for testing xtuml.tools.NodePrintVisitor
+    '''
+    def __init__(self):
+        self.children = list()
+        
+
+class TestNodePrinter(unittest.TestCase):
+    '''
+    Test the walker and visitor in xtuml.tools
+    '''
+    def test_node(self):
+        w = xtuml.tools.Walker()
+        w.visitors.append(xtuml.tools.NodePrintVisitor())
+        
+        root = Node()
+        root.children.append(Node())
+        root.children.append(Node())
+        
+        w.accept(root)
+        
+        s = sys.stdout.getvalue()
+        expected  = 'Node\n'
+        expected += '  Node\n'
+        expected += '  Node\n'
+        
+        self.assertEqual(s, expected)
+        
+    def test_generic_class(self):
+        w = xtuml.tools.Walker()
+        w.visitors.append(xtuml.tools.NodePrintVisitor())
+        
+        w.accept(self)
+        
+        s = sys.stdout.getvalue()
+        
+        self.assertEqual(s, 'TestNodePrinter\n')
+
+
+if __name__ == "__main__":
+    unittest.main()
+
