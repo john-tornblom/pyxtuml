2b18891502252b4aff9f464a77ef99baf9139312
John Törnblom <john.tornblom@gmail.com>
2016-01-11 19:46:13 +0000

    import: actually use the PYTHONPATH when searching for models

--- bridgepoint/imp.py
+++ bridgepoint/imp.py
@@ -34,7 +34,8 @@ class ModelImporter(object):
         model_name = names[-1]
         
         for f in paths:
-            path = os.path.join('models', model_name)
+            path = os.path.join(os.path.realpath(f), 'models')
+            path = os.path.join(path, model_name)
             source = os.path.join(path, model_name + '.xtuml')
             if _check_magic(source):
                 return cls(path)
