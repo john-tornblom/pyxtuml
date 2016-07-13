9bf66f161d305dc737dd7f723340629c2dda2448
John Törnblom <john.tornblom@gmail.com>
2016-07-13 20:21:54 +0000

    examples: cosmetics

--- examples/gen_ooaofooa_schema.py
+++ examples/gen_ooaofooa_schema.py
@@ -5,6 +5,7 @@
 import sys
 import logging
 import re
+import xtuml
 
 from xtuml import navigate_one as one
 from xtuml import navigate_many as many
@@ -34,28 +35,32 @@ def remove_attr(o_attr):
             return True
     
 
-logging.basicConfig(level=logging.INFO)
-
-
-if len(sys.argv) < 2:
-    print('')
-    print('  usage: %s <path to ooaofooa model folder>' % sys.argv[0])
-    print('')
-    sys.exit(1)
-
-
-loader = ooaofooa.Loader()
-for filename in sys.argv[1:]:
-    loader.filename_input(filename)
-
-m = loader.build_metamodel()
-c = loader.build_component()
-
-for o_obj in m.select_many('O_OBJ'):
-    for o_attr in many(o_obj).O_ATTR[102](remove_attr):
-        logger.info('Filtering %s.%s' % (o_obj.Key_Lett, o_attr.Name))
-        metaclass = c.find_metaclass(o_obj.Key_Lett)
-        metaclass.delete_attribute(o_attr.Name)
+def main():
+    if len(sys.argv) < 2:
+        logger.info('usage: %s <path to ooaofooa model folder>' % sys.argv[0])
+        sys.exit(1)
         
+    loader = ooaofooa.Loader()
+    for filename in sys.argv[1:]:
+        loader.filename_input(filename)
+    
+    m = loader.build_metamodel()
+    c = loader.build_component()
+    
+    for o_obj in m.select_many('O_OBJ'):
+        for o_attr in many(o_obj).O_ATTR[102](remove_attr):
+            logger.info('Filtering %s.%s' % (o_obj.Key_Lett, o_attr.Name))
+            metaclass = c.find_metaclass(o_obj.Key_Lett)
+            metaclass.delete_attribute(o_attr.Name)
+            
+    xtuml.persist_schema(c, '/dev/stdout')
 
-#xtuml.persist_database(c, '/dev/stdout')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
+    
+
+
+
+
