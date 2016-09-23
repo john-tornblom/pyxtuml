ce7016bac36b7bd7f2c7e169d64c7bac0d58ff04
John Törnblom <john.tornblom@gmail.com>
2016-09-23 08:38:06 +0200

    bump version

--- xtuml/version.py
+++ xtuml/version.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2016 John Törnblom
 
 name = 'pyxtuml'
-date = '2016-09-20'
+date = '2016-09-23'
 version = '0.9'
-release = '0.9.1'
+release = '0.9.2'
 
 complete_string = '%s v%s (%s)' % (name, release, date)
